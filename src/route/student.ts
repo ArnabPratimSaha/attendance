eb5e2e0e4b939c9cfcd2e77e644f67f60ee23fa3
arnab pratim saha <arnabpratimsaha@gmail.com>
2022-04-24 17:13:35 +0530

    changed user/class route

--- src/route/student.ts
+++ src/route/student.ts
@@ -25,30 +25,60 @@ router.get('/',async(req:CustomRequest,res:CustomResponse,next:NextFunction)=>{
         next(error);
     }
 })
-router.patch('/',userHandler,teacherHandler,async(req:CustomRequest,res:CustomResponse,next:NextFunction)=>{
+router.post('/',userHandler,teacherHandler,async(req:teacherRequest,res:CustomResponse,next:NextFunction)=>{
     try {
-        const id:string|undefined=req.body.id?.trim();
-        const name:string|undefined=req.body.name?.toString().trim();
-        const roll:string|undefined=req.body.roll?.toString().trim();
-        if(!id||!name||!roll)return next(new CustomError('id roll roll missing',400));
-        const student=await StudentModel.findOneAndUpdate({id:id},{$set:{name:name ,roll:roll} });
-        if(!student)return next(new CustomError('Student not found',404));
-        return res.sendStatus(200);
+        const name:string|undefined=req.body.name;
+        const roll:string|undefined=req.body.roll;
+        if( !name || !roll)return next(new CustomError('missing field name or roll',400));
+        if(!req.classData)return next(new CustomError('Class not found',404));
+        const student=new StudentModel({
+            id:uuidv4(),
+            name:name,
+            roll:roll,
+            classId:req.classData.id,
+            attendanceArray:new Array<boolean>(req.classData.attendanceArray.length).fill(false),
+        });
+        const classData=await ClassModel.findOne({id:req.classData.id});
+        if(!classData)return next('No Class found');
+        await student.save();
+        classData.students.push(student.id);
+        await classData.save();
+        return res.status(200).json({ ...student.toObject(),accesstoken:req.accesstoken })
     } catch (error) {
         next(error);
     }
 })
-router.delete('/',userHandler,teacherHandler,async(req:CustomRequest,res:CustomResponse,next:NextFunction)=>{
+router.patch('/',userHandler,teacherHandler,async(req:teacherRequest,res:CustomResponse,next:NextFunction)=>{
     try {
-        const id:string|undefined=req.body.id?.trim();
-        if(!id)return next(new CustomError('id missing',400));
+        const name:string|undefined=req.body.name;
+        const roll:string|undefined=req.body.roll;
+        const id:string|undefined=req.body.id;
+        if( !name || !roll ||  !id)return next(new CustomError('missing field name or roll or id',400));
+        if(!req.classData)return next(new CustomError('Class not found',404));
+        const student=await StudentModel.findOne({id:id});
+        if(!student)return next(new CustomError('Student not found',404));
+        student.name=name;
+        student.roll=roll;
+        await student.save();
+        return res.status(200).json({ ...student.toObject(),accesstoken:req.accesstoken })
+    } catch (error) {
+        next(error);
+    }
+})
+router.delete('/',userHandler,teacherHandler,async(req:teacherRequest,res:CustomResponse,next:NextFunction)=>{
+    try {
+        const id:string|undefined=req.body.id;
+        if(!id)return next(new CustomError('missing field id',400));
+        if(!req.classData)return next(new CustomError('Class not found',404));
+        const classData=await ClassModel.findOne({id:req.classData.id});
+        if(!classData)return next('No Class found');
         const student=await StudentModel.findOneAndDelete({id:id});
         if(!student)return next(new CustomError('Student not found',404));
-        const classData=await ClassModel.findOneAndUpdate({id:student.classId},{ $pull :{students:student.id }})
-        return res.sendStatus(200);
+        classData.students=classData.students.filter(i=>i!==student.id);
+        await classData.save();
+        return res.status(200).json({ ...student.toObject(),accesstoken:req.accesstoken })
     } catch (error) {
         next(error);
     }
 })
-
 export default router;
